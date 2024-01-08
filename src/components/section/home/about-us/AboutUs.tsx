164f7b4fa92969bfb942fbe0c2f5f0a4a5aa08ea
Diyor11 <diyorq3arshi@gmail.com>
2024-01-08 18:24:38 +0500

    home page finish

--- src/components/section/home/about-us/AboutUs.tsx
+++ src/components/section/home/about-us/AboutUs.tsx
@@ -6,6 +6,7 @@ import {
   Description,
   SubTitle,
   SubTitleLine,
+  TeemImage,
   Title,
 } from "./about-us.s";
 import Styles from "@/styles";
@@ -18,44 +19,59 @@ export const AboutUs: FC<IAboutUsProps> = (props) => {
   return (
     <AboutUsWrap>
       <Styles.Container>
-        <AboutImgBlock>
-          <img src="/images/about-us.png" alt="about-us" />
-        </AboutImgBlock>
-        <Styles.Column
-          width="100%"
-          direction="column"
-          content="center"
-          items="flex-start"
-          gap={32}
-        >
-          <Styles.Column
-            width="100%"
-            direction="column"
-            items="flex-start"
-            gap={14}
-          >
-            <Styles.Column direction="column" items="flex-start" gap={12}>
-              <SubTitle>About us</SubTitle>
-              <SubTitleLine />
+        <Styles.Column width="100%" items="center" gap={48}>
+          <Styles.Row size={{xs: 12, md: 3}} content={"center"} difference={{xs: 0, md: 24}}>
+            <AboutImgBlock>
+              <TeemImage
+                width={430}
+                height={320}
+                src="/images/about-us.png"
+                alt="about-us"
+              />
+            </AboutImgBlock>
+          </Styles.Row>
+          <Styles.Row size={{xs: 12, md: 9}} difference={{xs: 0, md: 24}}>
+            <Styles.Column
+              width="100%"
+              direction="column"
+              content="center"
+              items="flex-start"
+              gap={32}
+            >
+              <Styles.Column
+                width="100%"
+                direction="column"
+                items="flex-start"
+                gap={14}
+              >
+                <Styles.Column direction="column" items="flex-start" gap={12}>
+                  <SubTitle>About us</SubTitle>
+                  <SubTitleLine />
+                </Styles.Column>
+                <Styles.Column
+                  width="100%"
+                  direction="column"
+                  items="flex-start"
+                >
+                  <Title>О компании</Title>
+                  <Description>
+                    Компания «ABSOLUTE ENERGY» основана в 2016 г. и является
+                    официальным дилером торговых марок AKSA, Cummins, RID,
+                    GENPOWER и Mitsubishi на территории Республики Узбекистан.
+                    На сегодняшний день продукция компании широко востребована в
+                    нефтегазовой отрасли, а также в сегментах телекоммуникации,
+                    гостиничной и банковской системы. В компании имеется
+                    достаточное количество обученных и сертифицированных
+                    сервисных инженеров для оказания гарантийной после
+                    гарантийной поддержки оборудования в режиме 24/7
+                  </Description>
+                </Styles.Column>
+              </Styles.Column>
+              <AboutBtn>
+                Подробнее <Icons.arrowRight.Broken />
+              </AboutBtn>
             </Styles.Column>
-            <Styles.Column width="100%" direction="column" items="flex-start">
-              <Title>О компании</Title>
-              <Description>
-                Компания «ABSOLUTE ENERGY» основана в 2016 г. и является
-                официальным дилером торговых марок AKSA, Cummins, RID, GENPOWER
-                и Mitsubishi на территории Республики Узбекистан. На сегодняшний
-                день продукция компании широко востребована в нефтегазовой
-                отрасли, а также в сегментах телекоммуникации, гостиничной и
-                банковской системы. В компании имеется достаточное количество
-                обученных и сертифицированных сервисных инженеров для оказания
-                гарантийной после гарантийной поддержки оборудования в режиме
-                24/7
-              </Description>
-            </Styles.Column>
-          </Styles.Column>
-          <AboutBtn>
-            Подробнее <Icons.arrowRight.Broken />
-          </AboutBtn>
+          </Styles.Row>
         </Styles.Column>
       </Styles.Container>
     </AboutUsWrap>
